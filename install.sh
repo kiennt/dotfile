fa70d101805b586de1dfed3d15b39f29e0c1e89f
kiennt <trungkien2288@gmail.com>
2013-07-01 16:06:12 +0700

    Fix name error in gitconfig

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-rm -rf $HOME/.vim $HOME/.vimrc $HOME/.inputrc $HOME/.bashrc $HOME/.gitcolor
+rm -rf $HOME/.vim $HOME/.vimrc $HOME/.inputrc $HOME/.bashrc $HOME/.gitconfig
 
 ln -s `pwd`/vim $HOME/.vim
 ln -s `pwd`/vim/vimrc $HOME/.vimrc
 ln -s `pwd`/inputrc $HOME/.inputrc
 ln -s `pwd`/bashrc $HOME/.bashrc
-ln -s `pwd`/gitcolor $HOME/.gitcolor
+ln -s `pwd`/gitconfig $HOME/.gitconfig
