3e128bd3faa3bba96c1178941c1c00c14ff00d96
kiennt <trungkien2288@gmail.com>
2014-10-31 13:00:57 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -2,16 +2,31 @@
 
 APP_DIR=$HOME/.kiennt_dotfile
 
-if [ ! -d "$APP_DIR" ]; then
-    echo "Installing dotfile for the first time"
-    git clone https://github.com/skwp/dotfiles.git "$APP_DIR"
+function copy_files {
     cd "$APP_DIR"
     for f in .*; do
         if [[ $f != ".git"  && $f != "." && $f != ".." && $f != ".gitmodules" ]]; then
             cp -r $f $HOME
         fi
     done
+}
 
-else
-    echo "dotfile is already installed"
-fi
+function install {
+    echo "Installing dotfile for the first time"
+    git clone https://github.com/skwp/dotfiles.git "$APP_DIR"
+    copy_files
+}
+
+
+case $1 in
+    copy)
+        copy_files
+        ;;
+    *)
+        if [ ! -d "$APP_DIR" ]; then
+            install
+        else
+            echo "dotfile is already installed"
+        fi
+        ;;
+esac
